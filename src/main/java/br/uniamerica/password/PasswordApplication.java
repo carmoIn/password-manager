bbfaf7e465203b6c2667a06bb61961b6fd21fb39
Helcio L. do Carmo Junior <helcio@carmo.in>
2022-10-14 17:00:45 -0300

    feat: Add user validator

--- src/main/java/br/uniamerica/password/PasswordApplication.java
+++ src/main/java/br/uniamerica/password/PasswordApplication.java
@@ -1,13 +1,29 @@
 package br.uniamerica.password;
 
+import br.uniamerica.password.validator.UserValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.Validator;
 
 @SpringBootApplication
-public class PasswordApplication {
+public class PasswordApplication implements RepositoryRestConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(PasswordApplication.class, args);
     }
 
+    @Override
+    public void configureValidatingRepositoryEventListener(
+            ValidatingRepositoryEventListener validatingListener) {
+        addValidator(new UserValidator(), validatingListener);
+    }
+
+    private void addValidator(Validator validator,
+                              ValidatingRepositoryEventListener validatingListener) {
+        validatingListener.addValidator("beforeCreate", validator);
+        validatingListener.addValidator("beforeSave", validator);
+        validatingListener.addValidator("beforeLinkSave", validator);
+    }
 }
---
+++ src/main/java/br/uniamerica/password/validator/UserValidator.java
@@ -0,0 +1,57 @@
+package br.uniamerica.password.validator;
+
+import br.uniamerica.password.entity.User;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.validation.Errors;
+
+@Configurable
+public class UserValidator extends AbstractValidator<User> {
+    public UserValidator() {
+        super(User.class);
+    }
+
+    @Override
+    public void validate(Object obj, Errors errors) {
+        User user = (User) obj;
+
+        validateName(user, errors);
+        validateEmail(user, errors);
+        validateUsername(user, errors);
+        validatePassword(user, errors);
+    }
+
+    private void validateName(User user, Errors errors) {
+        String name = user.getName();
+        validateString("name", name, errors);
+    }
+
+    private void validateEmail(User user, Errors errors) {
+        String email = user.getEmail();
+        if (!validateNull("email", email, errors)) {
+            // Cannot do any more validations if the field is null
+            return;
+        }
+
+        if (!email.contains("@")) {
+            errors.rejectValue("email", "email.invalid",
+                    "Email must be valid!");
+        }
+
+        user.setEmail(user.getEmail().toLowerCase());
+    }
+
+    private void validateUsername(User user, Errors errors) {
+        String username = user.getUsername();
+
+        validateString("username", username, errors);
+        if (username == null) {
+            // Cannot do any more validations if the field is null
+            return;
+        }
+    }
+
+    private void validatePassword(User user, Errors errors) {
+        String password = user.getPassword();
+        validateString("password", password, errors);
+    }
+}
