44e2f07fe42673adaec6ff970c6a9878f351a3c7
Helcio L. do Carmo Junior <helcio@carmo.in>
2022-10-14 21:20:52 -0300

    fix: Fix PasswordEntity user join

--- src/main/java/br/uniamerica/password/entity/Password.java
+++ src/main/java/br/uniamerica/password/entity/Password.java
@@ -1,6 +1,8 @@
 package br.uniamerica.password.entity;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
@@ -8,16 +10,20 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Table(name = "passwords")
 public class Password extends AbstractEntity {
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "user_id")
+    @Getter @Setter
+    @JoinColumn(nullable = false, name = "user_id")
+    @ManyToOne(fetch = FetchType.EAGER)
     private User user;
 
+    @Getter @Setter
     @Column(name = "name", nullable = false)
     private String name;
 
+    @Getter @Setter
     @Column(name = "site", nullable = false)
     private String site;
 
+    @Getter @Setter
     @Column(name = "password", nullable = false)
     private String password;
 }
